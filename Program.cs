8a3ff63223ae111214d77c559e326a5c5ed103c0
Kieran Caulfield <Kieran.Caulfield@birkettlong.co.uk>
2020-04-01 16:58:28 +0100

    ADDED XML WRITER AND SQL PARAMETER ON MATTER CODE

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Data.Odbc;
+
 
 namespace SQL2XML
 {
@@ -15,6 +20,7 @@ namespace SQL2XML
     PUB.MATDB.IMPLEMENTATION,
     PUB.MATDB.""DATE-OPENED"",
     PUB.MATDB.""MT-TYPE"",
+    PUB.CLIDB.CONTACT1,
     PUB.HISTORY.DESCRIPTION As ""HST-DESCRIPTION"",
     PUB.HISTORY.""LEVEL-FEE-EARNER"",
     PUB.HISTORY.""DOCUMENT-NAME"",
@@ -36,34 +42,137 @@ From
             And PUB.HISTORY.IMPLEMENTATION = PUB.MATDB.IMPLEMENTATION
             And PUB.HISTORY.""DATE-INSERTED"" >= PUB.MATDB.""DATE-OPENED"" Inner Join
     PUB.""DOC-CONTROL"" On PUB.""DOC-CONTROL"".""DOC-ID"" = PUB.HISTORY.""DOCUMENT-NAME"" Inner Join
-    PUB.""FILE-LOCATION"" On PUB.""FILE-LOCATION"".""LOC-NAME"" = PUB.""DOC-CONTROL"".""ST-LOCATION""
+    PUB.""FILE-LOCATION"" On PUB.""FILE-LOCATION"".""LOC-NAME"" = PUB.""DOC-CONTROL"".""ST-LOCATION"" Inner Join
+    PUB.CLIDB On PUB.CLIDB.""CL-CODE"" = PUB.MATDB.""CL-CODE""
 Where
-    PUB.MATDB.""MT-CODE"" = '081389-000002' And
+    PUB.MATDB.""MT-CODE"" = ? And
     PUB.HISTORY.""DOCUMENT-TYPE"" Is Not Null And
     PUB.HISTORY.""DOCUMENT-TYPE"" <> ''
+order by 
+    PUB.MATDB.""CL-CODE"",
+    PUB.MATDB.""MT-CODE"",
+    PUB.HISTORY.""DATE-INSERTED"" ASC
 ";
-            System.Data.Odbc.OdbcConnection conn = null;
-            System.Data.Odbc.OdbcDataReader reader = null;
+            OdbcConnection conn = null;
+            OdbcDataReader reader = null;
 
             try
             {
                 // open connection
-                conn = new System.Data.Odbc.OdbcConnection(connString);
+                conn = new OdbcConnection(connString);
                 conn.Open();
                 // execute the SQL
-                System.Data.Odbc.OdbcCommand cmd = new System.Data.Odbc.OdbcCommand(sql, conn);
+                OdbcCommand cmd = new OdbcCommand(sql, conn);
+                cmd.Parameters.Add("MatterIdentifier", OdbcType.VarChar).Value = "081389-000002";
                 reader = cmd.ExecuteReader();
 
                 Console.WriteLine("Database = {0} \nDriver = {1} \nQuery {2}\nConnection String = {3}\nServer Version = {4}\nDataSource = {5}",
                 conn.Database, conn.Driver, cmd.CommandText, conn.ConnectionString, conn.ServerVersion, conn.DataSource);
 
+                XmlDocument xmlDoc = new XmlDocument();
+
+                XmlNode rootNode = xmlDoc.CreateElement("SolcaseDocs");
+                xmlDoc.AppendChild(rootNode);
+
+                XmlNode clientNode = xmlDoc.CreateElement("Client");
+                    XmlAttribute clientCode = xmlDoc.CreateAttribute("CL-CODE");
+                    XmlAttribute clientName = xmlDoc.CreateAttribute("CL-NAME");
+
+                XmlNode matterNode = xmlDoc.CreateElement("Matter");
+                    XmlAttribute matterCode = xmlDoc.CreateAttribute("MT-CODE");
+                    XmlAttribute matterDesc = xmlDoc.CreateAttribute("MAT-DESCRIPTION");
+
+
+                Boolean clientNodeNotCreated = true;
+                String currentMatterCode= ""; // on change of matter we need to create another matter node
+
                 while (reader.Read())
                 {
+                    if(clientNodeNotCreated)
+                    {
+                        // only once used, turn off once processed                        
+                            clientCode.Value = reader["CL-CODE"].ToString();
+                            clientNode.Attributes.Append(clientCode);
+                        
+                            clientName.Value = reader["CONTACT1"].ToString();
+                            clientNode.Attributes.Append(clientName);
+                        rootNode.AppendChild(clientNode);
+                        clientNodeNotCreated = false;
+                    }
+
+
+                    if(currentMatterCode != reader["MT-CODE"].ToString())
+                    {
+                        // the matter code has changed, create a new Matter Node                       
+                            matterCode.Value = reader["MT-CODE"].ToString();
+                            matterNode.Attributes.Append(matterCode);
+                        
+                            matterDesc.Value = reader["MAT-DESCRIPTION"].ToString();
+                            matterNode.Attributes.Append(matterDesc);
+                        clientNode.AppendChild(matterNode);
+                    }
+
+                    XmlNode soldocNode = xmlDoc.CreateElement("SolDoc");
+                        XmlAttribute histDesc = xmlDoc.CreateAttribute("HST-DESCRIPTION");
+                        XmlAttribute histNo = xmlDoc.CreateAttribute("HISTORY-NO");
+                        XmlAttribute docName = xmlDoc.CreateAttribute("DOCUMENT-NAME");
+                        XmlAttribute solcaseDocType = xmlDoc.CreateAttribute("DOCUMENT-TYPE");
+                        XmlAttribute dateInserted = xmlDoc.CreateAttribute("DATE-INSERTED");
+                        XmlAttribute histFE = xmlDoc.CreateAttribute("LEVEL-FEE-EARNER");
+                        XmlAttribute docGroup = xmlDoc.CreateAttribute("DOC-GROUP");
+                        XmlAttribute stLocation = xmlDoc.CreateAttribute("ST-LOCATION");
+                        XmlAttribute subPath = xmlDoc.CreateAttribute("SUB-PATH");
+                        XmlAttribute actualDocType = xmlDoc.CreateAttribute("DOC-TYPE");
+                        XmlAttribute docExt = xmlDoc.CreateAttribute("EXTENSION");
+                        XmlAttribute docVersion = xmlDoc.CreateAttribute("VERSION");
+                        XmlAttribute fileLoc = xmlDoc.CreateAttribute("LOC-DESCRIPTION");
+                        XmlAttribute fileLocType = xmlDoc.CreateAttribute("LOC-TYPE");
+
+                        // assign values from result set reader
+                        histNo.Value = reader["HISTORY-NO"].ToString();
+                        histDesc.Value = reader["HST-DESCRIPTION"].ToString();
+                        docName.Value = reader["DOCUMENT-NAME"].ToString();
+                        solcaseDocType.Value = reader["DOCUMENT-TYPE"].ToString();
+                        dateInserted.Value = reader["DATE-INSERTED"].ToString();
+                        histFE.Value = reader["LEVEL-FEE-EARNER"].ToString();
+                        docGroup.Value = reader["DOC-GROUP"].ToString();
+                        stLocation.Value = reader["ST-LOCATION"].ToString();
+                        subPath.Value = reader["SUB-PATH"].ToString();
+                        actualDocType.Value = reader["DOC-TYPE"].ToString();
+                        docExt.Value = reader["EXTENSION"].ToString();
+                        docVersion.Value = reader["VERSION"].ToString();
+                        fileLoc.Value = reader["LOC-DESCRIPTION"].ToString();
+                        fileLocType.Value = reader["LOC-TYPE"].ToString();
+
+                        // assign attributes to soldocNode
+                        soldocNode.Attributes.Append(histNo);
+                        soldocNode.Attributes.Append(histDesc);
+                        soldocNode.Attributes.Append(docName);
+                        soldocNode.Attributes.Append(solcaseDocType);
+                        soldocNode.Attributes.Append(dateInserted);
+                        soldocNode.Attributes.Append(histFE);
+                        soldocNode.Attributes.Append(docGroup);
+                        soldocNode.Attributes.Append(stLocation);
+                        soldocNode.Attributes.Append(subPath);
+                        soldocNode.Attributes.Append(actualDocType);
+                        soldocNode.Attributes.Append(docExt);
+                        soldocNode.Attributes.Append(docVersion);
+                        soldocNode.Attributes.Append(fileLoc);
+                        soldocNode.Attributes.Append(fileLocType);
+
+                    // append node to matter
+                    matterNode.AppendChild(soldocNode);
+
+                    // flush xml doc
+                    
+
                     Console.WriteLine("{0}|{1}|{2}",
                         reader["MT-CODE"],
                         reader["HST-DESCRIPTION"],
                         reader["ST-LOCATION"]);
                 }
+
+                xmlDoc.Save("test-doc.xml");
             }
             catch (Exception e)
             {
